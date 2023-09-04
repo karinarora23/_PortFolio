23b70f49694ad1cd9ae73363c72868b9e8a3c583
KARINA ARORA <142146401+karinarora23@users.noreply.github.com>
2023-09-04 21:38:31 +0530

    Update RightCol.jsx

--- PortFolio-Website-main/portfolio/src/components/RightCol.jsx
+++ PortFolio-Website-main/portfolio/src/components/RightCol.jsx
@@ -4,8 +4,8 @@ function RightCol({className}) {
     return (
         <ul className={`${className}`}>
             <li className="col-content rotate-90 transform hover:-translate-y-1 transition ease-in">
-                <a target="_blank" rel="noreferrer" href="mailto:mahansh21334@iiitd.ac.in" className='hover:text-red-500 flex justify-center items-center '>
-                <span>mahansh21334@iiitd.ac.in</span>
+                <a target="_blank" rel="noreferrer" href="mailto:karinarora23@gmail.com" className='hover:text-red-500 flex justify-center items-center '>
+                <span>karinarora23@gmail.com</span>
                 <div className="inline-block w-1 h-40 -rotate-90 translate-x-24 rounded-xl bg-red-200"></div>
                 </a>
                  
